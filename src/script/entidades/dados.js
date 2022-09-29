8a1996d427c5d007e679f502ede8398e5a647616
Bruno Peçanha <brunoitdesenv@gmail.com>
2022-09-29 18:14:31 -0300

    - Estrutura base das classes de entidades - Estrutura inicial das classes de serviços;

---
+++ src/script/entidades/dados.js
@@ -0,0 +1,9 @@
+class dados {
+    dadoUm;
+    dadoDois;
+
+   rolarDados() {      
+        dadoUm =  Math.floor(Math.random() * 6) + 1;
+        dadoDois = Math.floor(Math.random() * 6) + 1;   
+    }
+}
---
+++ src/script/entidades/jogador.js
@@ -0,0 +1,11 @@
+class Jogador{
+
+    avatar;
+    cor;
+
+    constructor(avatar, cor) {
+        this.avatar = avatar;
+        this.cor= cor;    
+    }
+
+}
